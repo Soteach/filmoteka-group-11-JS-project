1a317603cfa4440c18956f0f580757cb6faa510c
SlavaHlushko <slavahlushko90@gmail.com>
2022-10-29 08:58:28 +0300

    search and render

--- src/js/getBySearch.js
+++ src/js/getBySearch.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
+
 const refs = {
   searchForm: document.querySelector('.header-search__form'),
   searchInput: document.querySelector('.header-search-input'),
+  container: document.querySelector('.header__conteiner'),
 };
-console.log(searchForm);
+
 let currentPage = 1;
 
 let markup = '';
@@ -10,8 +13,9 @@ let markup = '';
 
 async function fetchMovies(inputQuery, currentPage) {
   const mainUrl = `https://api.themoviedb.org/3/search/movie`;
-  const filters = `?api_key=${c3923fa38d2dd62131b577696cc2f23f}&query=${inputQuery}&page=${currentPage}`;
+  const filters = `?api_key=c3923fa38d2dd62131b577696cc2f23f&query=${inputQuery}&page=${currentPage}`;
   const response = await fetch(`${mainUrl}${filters}`);
+  console.log(response);
   return response.json();
 }
 
@@ -22,8 +26,26 @@ async function onSearch(event) {
   event.preventDefault();
 
   inputQuery = refs.searchInput.value;
+  console.log(inputQuery);
+  fetchMovies(inputQuery, 1);
+  
+  renderMoviesList(1);
 }
 
+
+async function getGenres() {
+  try {
+    const resGenres = await axios.get(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=c3923fa38d2dd62131b577696cc2f23f`
+    );
+    return await resGenres.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+let genreList1 = getGenres();
+// console.log(genreList1);
  async function renderMoviesList(pageNumber) {
   currentPage = pageNumber;
 
@@ -40,10 +62,8 @@ async function onSearch(event) {
           release_date,
           vote_average,
         }) => {
-          const genresList = JSON.parse(localStorage.getItem('genres'));
-          const genres = genre_ids.map(item => {
-            return getGenreById(item, genresList);
-          });
+          const genresList = genreList1;
+          const genres = genre_ids.map(item => {return item.genresList});
           let genresMarkup = '';
           if (genres.length === 0) {
             genresMarkup = 'No genres';
@@ -71,9 +91,10 @@ async function onSearch(event) {
             </div>
         </li>`;
         }
-      );
-      return markup;
+      ).join();
+      console.log(markup);
     }
+    refs.container.innerHTML = markup;
   });
 }
 
