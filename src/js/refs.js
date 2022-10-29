0a9e743da4b98c6aeb2fb26c8677f75256f1e4c7
Sasha.Solo <sasha.solomannik@gmail.com>
2022-10-29 06:19:45 +0300

    add fetch and markup

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,4 +2,5 @@ export const refs = {
   searchForm: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
   spinner: document.querySelector('.loader'),
+  gallerySet: document.querySelector('.gallery__set'),
 };
