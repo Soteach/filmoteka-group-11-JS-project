9ea227ff12be6b746814176c9ca17734c108486e
Kateryna Kasian <kkasyan1@gmail.com>
2022-10-30 01:12:31 +0300

    added firebase

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,4 +3,13 @@ export const refs = {
   gallery: document.querySelector('.gallery'),
   spinner: document.querySelector('.loader'),
   gallerySet: document.querySelector('.gallery__set'),
+  signInBtn: document.querySelector('.sign-in_js'),
+  closeAuthBtn: document.querySelector('[data-modal-close]'),
+  authModal: document.querySelector('[data-modal]'),
+  authForm: document.querySelector('#auth-form'),
+  sendSignInInfo: document.querySelector('.signin__btn'),
+  sendSignUpInfo: document.querySelector('.signup__btn'),
+  btnLogout: document.querySelector('.log-out_js'),
+  btnGoogle: document.querySelector('.google__btn'),
+  authBackdrop: document.querySelector('.js-backdrop'),
 };
