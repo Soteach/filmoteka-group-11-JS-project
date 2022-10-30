b7f65990695de769be528128842149c2b533d428
SlavaHlushko <slavahlushko90@gmail.com>
2022-10-30 21:57:20 +0200

    add notifyError

--- src/js/getBySearch.js
+++ src/js/getBySearch.js
@@ -1,10 +1,12 @@
+
 import axios from 'axios';
+import { spinnerPlay, spinnerStop } from './spinner';
 
 const refs = {
   searchForm: document.querySelector('.header-search__form'),
   searchInput: document.querySelector('.header-search-input'),
-
   container: document.querySelector('.gallery__set'),
+  notifyError: document.querySelector('.form-text'),
 };
 
 import { renderFilmsMarkup } from './renderMarkup';
@@ -18,13 +20,8 @@ async function fetchMovies(inputQuery, currentPage) {
   const filters = `?api_key=c3923fa38d2dd62131b577696cc2f23f&query=${inputQuery}&page=${currentPage}`;
 
   const response = await axios.get(`${mainUrl}${filters}`);
-  // console.log(response);
-
-  // console.log(response.json());
 
   const { data } = response;
-
-  // console.log(data.results);
 
   return data.results;
 }
@@ -37,33 +34,12 @@ async function onSearch(event) {
 
   inputQuery = refs.searchInput.value;
 
-  // console.log(inputQuery);
-  // fetchMovies(inputQuery, 1);
   const data = await fetchMovies(inputQuery, 1);
+  if (data.length === 0) {
+    return (refs.notifyError.textContent =
+      'Search result not successful. Enter the correct movie name.');
+  }
 
   renderFilmsMarkup(data);
 }
 
-async function renderMoviesList(pageNumber) {
-  currentPage = pageNumber;
-}
-
-// async function addPagination() {
-//   await fetchMovies(inputQuery, currentPage).then(res => {
-//     totalPages = res.total_pages;
-//     return totalPages;
-//   });
-//   if (totalPages === 0) {
-//     refs.searchErrorNotif.textContent =
-//       'Search result not successful. Enter the correct movie name and try again';
-//     $(`#pagination-container`).pagination(`destroy`);
-//     refs.moviesList.innerHTML = ``;
-//     return;
-//   } else if (inputQuery === ``) {
-//     refs.searchErrorNotif.textContent = 'Please enter the name of the movie';
-//     $(`#pagination-container`).pagination(`destroy`);
-//     refs.moviesList.innerHTML = ``;
-//     return;
-//   }
-//   refs.searchErrorNotif.textContent = '';
-// }
