ccf23459d9b70a1f54a68030edf17e6c28fcd0ff
Sergij Suprunenko <104130197+Soteach@users.noreply.github.com>
2022-11-03 21:51:25 +0200

    fix index js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './js/fetchApi';
 import './js/renderMarkup';
-import './js/startPagStorage';
+// import './js/startPagStorage';
 
 import './js/nightMode';
 import './js/firebase/auth-modal-open';
